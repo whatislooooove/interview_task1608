804fa66126c9ad6eaf23909d7de8f21d3d7c1250
whatislove <a@a.a>
2024-08-17 13:45:47 +0300

    Bootstrap installed

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div>
+                <a href="#">Добавить продукт</a>
+            </div>
+            <div>
+                <a href="#">Получить продукты iPhone</a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
